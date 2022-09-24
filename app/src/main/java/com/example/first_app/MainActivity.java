15480bfe875b930500b9954cb2af7590f37dea29
Yazhe Feng <yazhe.fyz@gmail.com>
2022-09-23 21:42:34 -0700

    Assignment 1

--- app/src/main/java/com/example/first_app/MainActivity.java
+++ app/src/main/java/com/example/first_app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.first_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -11,13 +12,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MyApp";
+    private Button clicky;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         Button btn = (Button) findViewById(R.id.btnDoMagic);
+        clicky = (Button) findViewById(R.id.clickyButton);
 
         //Roast the button
         btn.setOnClickListener(new View.OnClickListener() {
@@ -27,6 +30,20 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "Yazhe Feng, feng.yaz@northeastern.edu", Toast.LENGTH_SHORT).show();
             }
         });
+
+        //new activity
+        clicky.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity2();
+            }
+        });
+    }
+
+    public void openActivity2() {
+        Intent intent = new Intent(this, Activity2.class);
+        startActivity(intent);
+
     }
 
 }
