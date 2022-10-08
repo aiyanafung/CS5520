f20df5d977e4b282cc13ea3094f2861dca6574e8
Yazhe Feng <yazhe.fyz@gmail.com>
2022-10-07 23:48:49 -0700

    fixed some bugs

--- app/src/main/java/com/example/first_app/Activity4.java
+++ app/src/main/java/com/example/first_app/Activity4.java
@@ -2,10 +2,12 @@ package com.example.first_app;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -36,9 +38,12 @@ public class Activity4 extends AppCompatActivity {
 
     PrimeThread thread;
 
+    private long onBackPressedTime;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_4);
 
         textViewLatestPrime = findViewById(R.id.activity4_latest_prime);
@@ -52,6 +57,7 @@ public class Activity4 extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 terminated = false;
+                currentNumber = START_NUMBER;
                 findingPrimes();
             }
         });
@@ -60,9 +66,23 @@ public class Activity4 extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 terminated = true;
+
+                textViewCurrentNumber.setText("");
             }
         });
 
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (onBackPressedTime + 2000 > System.currentTimeMillis()) {
+            super.onBackPressed();
+            finish();
+        } else {
+            Toast.makeText(getBaseContext(), "Press back again to exit", Toast.LENGTH_SHORT).show();
+        }
+        onBackPressedTime = System.currentTimeMillis();
     }
 
     private void findingPrimes() {
@@ -70,18 +90,23 @@ public class Activity4 extends AppCompatActivity {
         thread.start();
     }
 
+    private boolean isPrime(int number) {
+        // Corner cases
+        if (number <= 1)
+            return false;
+        if (number <= 3)
+            return true;
 
+        // This is checked so that we can skip
+        // middle five numbers in below loop
+        if (number % 2 == 0 || number % 3 == 0)
+            return false;
 
-    private boolean isPrime(long number) {
-        boolean isPrime = false;
-        for (int i = 2; i <= number / 2; ++i) {
-            if (number % i == 0) {
-                isPrime = true;
-                break;
-            }
-        }
+        for (int i = 5; i * i <= number; i = i + 6)
+            if (number % i == 0 || number % (i + 2) == 0)
+                return false;
 
-        return isPrime;
+        return true;
     }
 
     class PrimeThread extends Thread {
@@ -94,32 +119,33 @@ public class Activity4 extends AppCompatActivity {
             handler.post(new Runnable() {
                 @Override
                 public void run() {
-                    textViewCurrentNumber.setText(currentNumber);
+                    textViewCurrentNumber.setText(String.valueOf(currentNumber));
                 }
             });
 
             while (currentNumber < Integer.MAX_VALUE - 1 && !terminated) {
                 currentTime = System.currentTimeMillis();
-                boolean isPrime = isPrime(currentNumber);
-                if (currentTime - startTime > 2000) {
+                if(isPrime(currentNumber)) {
+                    Log.d("ACTIVITY_4", "isPrime: " + currentNumber);
+                    latestPrime = currentNumber;
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            textViewLatestPrime.setText(String.valueOf(latestPrime));
+                        }
+                    });
+                }
+
+                if (currentTime - startTime > 1000) {
                     startTime = System.currentTimeMillis();
 
                     handler.post(new Runnable() {
                         @Override
                         public void run() {
-                            textViewCurrentNumber.setText(currentNumber);
+                            textViewCurrentNumber.setText(String.valueOf(currentNumber));
                         }
                     });
 
-                    if(isPrime) {
-                        latestPrime = currentNumber;
-                        handler.post(new Runnable() {
-                            @Override
-                            public void run() {
-                                textViewLatestPrime.setText(latestPrime);
-                            }
-                        });
-                    }
                 }
 
                 currentNumber += INCREMENT;
